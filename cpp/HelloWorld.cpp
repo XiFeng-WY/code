3a4c336f5f393609abf4c7e0b88357b6a10d6fa2
XiFeng <1205771249@qq.com>
2024-04-20 21:54:35 +0800

    git add

---
+++ cpp/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    system("pause");//用于激活GBK编码
+    cout << "Hello, World!\n你好世界\n";
+    system("pause");
+    // system("pause") 用于保持窗口开启
+    return 0;
+}
---
+++ cpp/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    int a;
+    int b;
+    cin >> a >> b;
+    
+    cout << "Hello, World!\n";
+    cout << a << " " << b << endl; 
+    system("pause");
+    return 0;
+}
